7ca1acec8ae90bc74bd200015474cdcb2e9b0173
Keshava L <kesmurt0@publicisgroupe.net>
2023-05-04 11:25:34 -0500

    Login changes

--- src/Login/Login_form.tsx
+++ src/Login/Login_form.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+import "./LoginForm.css";
+import { Image } from "primereact/image";
 interface onLoginSuccess {
-    onLoginSuccessHandler(): any;
-  }
+  onLoginSuccessHandler(): any;
+}
 
-const Login_Form = (params:onLoginSuccess) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const Login_Form = (params: onLoginSuccess) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
@@ -15,32 +16,86 @@ const Login_Form = (params:onLoginSuccess) => {
   };
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <h1 style={{ fontSize: '1.5rem', marginBottom: '1rem' }}>Sign in to Outlook</h1>
-      <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', width: '300px' }}>
-        <label htmlFor="username" style={{ marginBottom: '0.5rem', fontWeight: 'bold' }}>Email or username</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-          style={{ marginBottom: '1rem', padding: '0.5rem', fontSize: '1rem' }}
-        />
-        <label htmlFor="password" style={{ marginBottom: '0.5rem', fontWeight: 'bold' }}>Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          style={{ marginBottom: '1rem', padding: '0.5rem', fontSize: '1rem' }}
-        />
-        <button type="submit" style={{ padding: '0.5rem', backgroundColor: '#0078D4', color: 'white', borderRadius: '4px', fontSize: '1rem', fontWeight: 'bold' }}>Sign in</button>
-        <div style={{ display: 'flex', alignItems: 'center', marginTop: '0.5rem' }}>
-          <input type="checkbox" id="remember-me" style={{ marginRight: '0.5rem' }} />
-          <label htmlFor="remember-me" style={{ fontSize: '0.875rem' }}>Remember me</label>
-          <a href="#" style={{ marginLeft: 'auto', fontSize: '0.875rem' }}>Need help?</a>
-        </div>
-      </form>
+    <div className="container-main-login">
+      <div className="left-side-login">
+        <h2> Remax Crystal</h2>        
+      </div>
+      <div className="right-side-login">
+        <h1 style={{ fontSize: "1.5rem", marginBottom: "1rem" }}>
+          Sign in to Remax
+        </h1>
+        <form
+          onSubmit={handleSubmit}
+          style={{ display: "flex", flexDirection: "column", width: "300px" }}
+        >
+          <label
+            htmlFor="username"
+            style={{ marginBottom: "0.5rem", fontWeight: "bold" }}
+          >
+            Email or username
+          </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            style={{
+              marginBottom: "1rem",
+              padding: "0.5rem",
+              fontSize: "1rem",
+            }}
+          />
+          <label
+            htmlFor="password"
+            style={{ marginBottom: "0.5rem", fontWeight: "bold" }}
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            style={{
+              marginBottom: "1rem",
+              padding: "0.5rem",
+              fontSize: "1rem",
+            }}
+          />
+          <button
+            type="submit"
+            style={{
+              padding: "0.5rem",
+              backgroundColor: "#0078D4",
+              color: "white",
+              borderRadius: "4px",
+              fontSize: "1rem",
+              fontWeight: "bold",
+            }}
+          >
+            Sign in
+          </button>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginTop: "0.5rem",
+            }}
+          >
+            <input
+              type="checkbox"
+              id="remember-me"
+              style={{ marginRight: "0.5rem" }}
+            />
+            <label htmlFor="remember-me" style={{ fontSize: "0.875rem" }}>
+              Remember me
+            </label>
+            <a href="#" style={{ marginLeft: "auto", fontSize: "0.875rem" }}>
+              Need help?
+            </a>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
